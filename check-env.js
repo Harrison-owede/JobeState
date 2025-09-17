9b30075982b4ec1c63de40932cb6c4f3a4a380e9
Harrison owede <harrisonowede3@gmail.com>
2025-09-17 19:36:13 +0100

    app.js

--- check-env.js
+++
@@ -1,8 +0,0 @@
-// check-env.js
-import 'dotenv/config';
-
-console.log("Raw MONGODB_URI value from .env:");
-console.log(process.env.MONGODB_URI);
-
-console.log("\nDebug view (JSON stringified, to catch hidden quotes/spaces):");
-console.log(JSON.stringify(process.env.MONGODB_URI));
